fa3f4180a5e097ed68e5f51dd7e106a4e9859c8d
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-03-04 12:50:22 +0900

    bit fix

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,7 +2,7 @@ from omegaconf import DictConfig
 
 from src.data.coco import CocoDataModule
 from src.data.img_dataset import H5DataModule
-from src.data.mdn_dataset import MDNRNNDataModule
+from src.data.mdn_dataset import WorldModelDataModule
 
 def get_data_module(data_cfg: DictConfig):
     
@@ -21,7 +21,7 @@ def get_data_module(data_cfg: DictConfig):
                           num_workers=data_cfg.num_workers)
     elif data_cfg.name == "mdn":
         print(f"Loading data module for {data_cfg.name} dataset")
-        return MDNRNNDataModule(data_dir=data_cfg.data_dir,
+        return WorldModelDataModule(data_dir=data_cfg.data_dir,
                                 batch_size=data_cfg.batch_size,
                                 seq_length=data_cfg.seq_length,
                                 num_workers=data_cfg.num_workers)
